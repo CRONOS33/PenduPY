1e012da767a4efd2ae0bf7beb9067df51ec1c5dc
Antony.Guillot <antony.guillot@cpe.fr>
2021-12-10 10:03:40 +0100

    pendu

--- Penduconsole.py
+++ Penduconsole.py
@@ -3,8 +3,9 @@
 #ce qui est améliorable:
 
 ##Modules
+from os import close
 import random as rd
-from tkinter.constants import N
+from tkinter.constants import N, TRUE
 
 ##Variables
 #alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
@@ -14,71 +15,156 @@ dictionnaire=["dragon","marmitte","joueur","violet","camion","chaise"]
 
 def choix(liste_mots):
     #cette fontion renvoi un élément aléatoir d'une liste.
-
     return rd.choice(liste_mots)
     
+def liste_mots():
+    #cration des mots avec un fichier txt
+    mots=open("Mots.txt","r")
+    dictionnaire=mots.readlines()
+    n=len(dictionnaire)
+    for i in range(n) :
+        element=dictionnaire[i]
+        element.lower()
+        element=list(element)
+        for lettre in element:
+            if lettre=="\n":
+                 element.remove("\n")
+        mot=""
+        for lettre in element:
+            mot=mot+lettre
+        
+        dictionnaire[i]=mot
+    mots.close()
+
+    return dictionnaire
+
+
 def choix_lettres(mot):
+    #creation du mot à trouver
     n=len(mot)
     liste_lettre=list(mot)
     nb_lettre_visible= n//7 +1
     lettres_visibles=[]
+    boucle=True
+    while boucle:
+        try :
+            difficulte=int(input("Entrez une version 0 (lettre du debut) ou 1 (une lettre pour 7 lettre dans le mot) "))
+            if difficulte==1 or difficulte==0:
+                boucle=False
+            else:
+                print("Vous devez rentrez 0 ou 1")
 
-    for i in range(nb_lettre_visible):
-        lettre=choix(liste_lettre)
-        if lettre not in lettres_visibles:
-            lettres_visibles.append(lettre)
+
+        except  ValueError:
+            print("Entrez un chiffre (0 ou 1)")
+
+
+    if difficulte==1:
+        for i in range(nb_lettre_visible):
+            lettre=choix(liste_lettre)
+            if lettre not in lettres_visibles:
+                lettres_visibles.append(lettre)
+
+        
+    elif difficulte==0:
+        lettres_visibles=liste_lettre[0]
+    else:
+        print("Choissisez entre 0 et 1 pas autre chose merci:        ")  
+        return choix_lettres(mot)
 
     for i in range(n):
-        if liste_lettre[i] not in lettres_visibles:
-            liste_lettre[i]=" _ "
+            if liste_lettre[i] not in lettres_visibles:
+                liste_lettre[i]=" _ "      
 
     return liste_lettre
 
 
 def pendu():
+    #cette fonction affiche une version du jeu en console
     alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     erreurs=8
     mot=choix(dictionnaire)
     liste_lettre=choix_lettres(mot)
     n=len(mot)
     lettres_tester=[]
+    for lettre in liste_lettre:
+        if lettre in alphabet:
+            lettres_tester=lettres_tester +[lettre]
  
     while erreurs!=0 and " _ " in liste_lettre:
         print()
         print(liste_lettre)
-        print("voici les lettres que vous avez essayé   ")
-        print(lettres_tester)
-        essai=str(input("entrer une lettre 'en minusule'  à essayer:     "))
-        if essai in alphabet:
-            if essai not in lettres_tester:
-                 lettres_tester.append(essai)
-           
-            if essai in mot and essai not in liste_lettre:
-                liste_indice=[]
-                for i in range(n):
-                    if essai==mot[i]:
-                        liste_indice.append(i)
-                for indice in (liste_indice):
-                    liste_lettre[indice]=essai
-            else:
-                print("Rater essaye encore")
-               
-                erreurs=erreurs-1
-                print("il vous reste")
-                print(erreurs)
-                print("possibilités") 
-        else:
-            print("rentrer une lettre en minuscules")
+        print("Il vous reste {a} possibilité ".format(a=erreurs))
+        print("Voici les lettres que vous avez essayé {a} ".format(a=lettres_tester))
+        boucle=True
+        while boucle:
+            try:
+                essai=str(input("Entrer une lettre 'en minusule'  à essayer:     "))
+
+                if essai in alphabet:
+                    boucle=False
+                    if essai not in lettres_tester:
+                        lettres_tester.append(essai)
+                    
+                        if essai in mot and essai not in liste_lettre:
+                            liste_indice=[]
+                            for i in range(n):
+                                if essai==mot[i]:
+                                    liste_indice.append(i)
+                            for indice in (liste_indice):
+                                liste_lettre[indice]=essai
+                        else:
+                            print("Rater cette lettre n'y est pas essaye encore")                    
+                            erreurs=erreurs-1
+                            
+                    else:
+                        print("ATTENTION vous avez déjà tester cette lettre ")
+                else:
+                    print("Rentrez une lettre en minuscules")
+
+            except ValueError:
+                print("Entrez ce que je vous demande svp , une lettre en minuscule")
+
     if erreurs==0:
-        print("la reponse était")
-        print(mot)
-        print("le pendu est mort")
-        return None
+        print("La reponse était {a} le pendu est mort try again".format(a=mot))
+        return 0
         
     else:
-        print("vous avez trouvez")
-        print(liste_lettre)
-        print("GG")
-        return None
+        print("Vous avez trouvé {a} GG".format(a=liste_lettre))
+        return 1
 
-pendu()
+
+
+def boucle_jeu():
+    boucle=True
+    while boucle:
+        try:
+            volonte=int(input("Pour jouer entre 1 sinon entrez 0: "))
+            if volonte==0 or volonte==1:
+                boucle=False
+            else:
+                print("J'ai dit 0 OU 1")
+        except  ValueError:
+            print("Non mais entrez un nombre au moins")
+        
+    volonte=1
+    while volonte==1:
+        pendu()
+        boucle=True
+        while boucle:
+            try:
+                volonte=int(input("Pour retry taper 1 0 pour arreter:    "))
+                if volonte == 0 or volonte==1:
+                    boucle=False
+                else:
+                    print("0 ou 1 !!")
+            except  ValueError:
+                print("Non mais entrez un nombre au moins")
+           
+
+            
+    print("BY BY")
+    return
+    
+dictionnaire=liste_mots()
+boucle_jeu()
